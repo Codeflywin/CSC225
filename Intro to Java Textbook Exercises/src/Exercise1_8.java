8b57c9ddad2ec1055234f2218a257fea6015f712
MKaczorowski <codeflywin@gmail.com>
2025-01-17 21:55:55 -0600

    Changes to 1_8

--- Intro to Java Textbook Exercises/src/Exercise1_8.java
+++ Intro to Java Textbook Exercises/src/Exercise1_8.java
@@ -3,8 +3,10 @@ public class Exercise1_8 {
 
 	public static void main(String[] args) {
 		// Display Perimeter
-System.out.println(2*6.5*3.14159);
+		System.out.println("Perimeter:");
+		System.out.println(2*6.5*3.14159);
        //Display Area
+		System.out.println("Area:");
 System.out.println(6.5*6.5*3.14159);
 	}
 
