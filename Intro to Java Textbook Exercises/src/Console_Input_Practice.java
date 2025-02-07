5fe9f15e7b1ec550e5c0932636bda600631a14d9
MKaczorowski <codeflywin@gmail.com>
2025-02-06 20:56:27 -0600

    Practice console exercise from textbook, self rewrite.

---
+++ Intro to Java Textbook Exercises/src/Console_Input_Practice.java
@@ -0,0 +1,25 @@
+// In order to allow console input, scanner class from java.util package needs to be added
+
+import java.util.Scanner; 
+
+public class Console_Input_Practice {
+
+	public static void main(String[] args) {
+		// Add the scanner object
+		Scanner input = new Scanner (System.in);
+		
+		// Have the user add the radius input through the console
+		System.out.println ("Enter a number for radius: ");
+		double radius = input.nextDouble();
+		
+		//Compute the area of the circle
+		double area = radius * radius * 3.14159;
+		
+		//Display the results
+		System.out.println ("The area for the circle of radius " + 
+		radius + " is " + area);
+		
+
+	}
+
+}
